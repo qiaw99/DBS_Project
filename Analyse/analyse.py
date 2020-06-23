f37b94826b5b92cddf69c428bf9656f521a59c06
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2020-06-23 14:36:38 +0200

    Update analyse.py

--- Analyse/analyse.py
+++ Analyse/analyse.py
@@ -10,7 +10,7 @@ with open("covid19.json", 'r') as f:
     
 record = {}
 
-# get pure python data structure in form 
+# get pure python data structure in form [{}, {}, ..., {}]
 data = json_str["records"]
 
 for dic in data: 
@@ -21,20 +21,27 @@ for dic in data:
         record[dic["countriesAndTerritories"]][1].append([dic["cases"], dic["deaths"]])
         
 #print(record)
+counter = 0
+for key in record.keys():
+    print(key)
+    dates = record[key][0]
 
-dates = record["Afghanistan"][0]
+    #print(dates)
+    xs = [datetime.strptime(d, '%d/%m/%Y').date() for d in dates]
+    temp = record[key][1]
+    ys = []
+    for i in range(len(temp)):
+        ys.append(int(temp[i][0]))
+    #print(ys)
 
-#print(dates)
-xs = [datetime.strptime(d, '%d/%m/%Y').date() for d in dates]
-temp = record["Afghanistan"][1]
-ys = []
-for i in range(len(temp)):
-    ys.append(int(temp[i][0]))
-#print(ys)
-
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%Y'))
-plt.gca().xaxis.set_major_locator(mdates.DayLocator())
-# Plot
-plt.plot(xs, ys)
-plt.gcf().autofmt_xdate()  
-plt.show()
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%Y'))
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+    # Plot
+    plt.title("The report of " + key)
+    plt.ylabel("Number of cases")
+    plt.xlabel("Date")
+    plt.plot(xs, ys)
+    plt.gcf().autofmt_xdate()  
+    plt.savefig("plot" + str(counter) + ".png")
+    counter += 1
+    plt.show()
