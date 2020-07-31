15f26a2040679401586529836712961b2ef19427
sopa40 <hazik991@gmail.com>
2020-07-31 17:10:52 +0300

    add interval drawing

--- website/assets/js/main.js
+++ website/assets/js/main.js
@@ -108,11 +108,11 @@ $(document).ready(function () {
         Object.keys(unsortedDailyCases).sort().forEach((key) => {
             dailyCases[key] = unsortedDailyCases[key]
         });
-        paintGraph(dailyCases, caseGraph);
-
         Object.keys(unsortedDailyDeath).sort().forEach((key) => {
             dailyDeath[key] = unsortedDailyDeath[key]
         });
+
+        paintGraph(dailyCases, caseGraph);
         paintGraph(dailyDeath, deathGraph);
         updateOptions();
     }
@@ -141,98 +141,6 @@ $(document).ready(function () {
 			},
 			options: {
 				responsive: true
-            },
-            plugins: {
-                zoom: {
-                    // Container for pan options
-                    pan: {
-                        // Boolean to enable panning
-                        enabled: true,
-            
-                        // Panning directions. Remove the appropriate direction to disable
-                        // Eg. 'y' would only allow panning in the y direction
-                        // A function that is called as the user is panning and returns the
-                        // available directions can also be used:
-                        //   mode: function({ chart }) {
-                        //     return 'xy';
-                        //   },
-                        mode: 'xy',
-            
-                        rangeMin: {
-                            // Format of min pan range depends on scale type
-                            x: null,
-                            y: null
-                        },
-                        rangeMax: {
-                            // Format of max pan range depends on scale type
-                            x: null,
-                            y: null
-                        },
-            
-                        // On category scale, factor of pan velocity
-                        speed: 20,
-            
-                        // Minimal pan distance required before actually applying pan
-                        threshold: 10,
-            
-                        // Function called while the user is panning
-                        onPan: function({chart}) { console.log(`I'm panning!!!`); },
-                        // Function called once panning is completed
-                        onPanComplete: function({chart}) { console.log(`I was panned!!!`); }
-                    },
-            
-                    // Container for zoom options
-                    zoom: {
-                        // Boolean to enable zooming
-                        enabled: true,
-            
-                        // Enable drag-to-zoom behavior
-                        drag: true,
-            
-                        // Drag-to-zoom effect can be customized
-                        // drag: {
-                        // 	 borderColor: 'rgba(225,225,225,0.3)'
-                        // 	 borderWidth: 5,
-                        // 	 backgroundColor: 'rgb(225,225,225)',
-                        // 	 animationDuration: 0
-                        // },
-            
-                        // Zooming directions. Remove the appropriate direction to disable
-                        // Eg. 'y' would only allow zooming in the y direction
-                        // A function that is called as the user is zooming and returns the
-                        // available directions can also be used:
-                        //   mode: function({ chart }) {
-                        //     return 'xy';
-                        //   },
-                        mode: 'xy',
-            
-                        rangeMin: {
-                            // Format of min zoom range depends on scale type
-                            x: null,
-                            y: null
-                        },
-                        rangeMax: {
-                            // Format of max zoom range depends on scale type
-                            x: null,
-                            y: null
-                        },
-            
-                        // Speed of zoom via mouse wheel
-                        // (percentage of zoom on a wheel event)
-                        speed: 0.1,
-            
-                        // Minimal zoom distance required before actually applying zoom
-                        threshold: 2,
-            
-                        // On category scale, minimal zoom level before actually applying zoom
-                        sensitivity: 3,
-            
-                        // Function called while the user is zooming
-                        onZoom: function({chart}) { console.log(`I'm zooming!!!`); },
-                        // Function called once zooming is completed
-                        onZoomComplete: function({chart}) { console.log(`I was zoomed!!!`); }
-                    }
-                }
             }
 
 		});
@@ -245,8 +153,21 @@ $(document).ready(function () {
                     .attr("value", value)
                     .text(value));
         });
-        $.each(dailyCases, function( key, value ) {
+        $.each(dailyCases, function(key, value ) {
             $('#day')
+                .append($("<option></option>")
+                    .attr("value", key)
+                    .text(key));
+        });
+
+        $.each(dailyCases, function( key, value ) {
+            $('#startDate')
+                .append($("<option></option>")
+                    .attr("value", key)
+                    .text(key));
+        });
+        $.each(dailyCases, function( key, value ) {
+            $('#endDate')
                 .append($("<option></option>")
                     .attr("value", key)
                     .text(key));
@@ -255,10 +176,44 @@ $(document).ready(function () {
 
     $("#queryForm").submit(function(e) {
         e.preventDefault();
-        handleRequest();
+        updateTable();
     });
     
-    function handleRequest() {
+    $("#graphForm").submit(function(e) {
+        e.preventDefault();
+        drawGraphs();
+    });
+
+    function drawGraphs() {
+        var startDate, endDate, dateList,startIndex, endIndex, newDateList, newDailyCases = {}, newDailyDeath = {};
+        let newCaseGraph = 'intervalCases';
+        let newDeathGraph = 'intervalDeath'
+        startDate = document.getElementById("startDate").value;
+        if (startDate === "") {
+            alert("You did not pick a country! Error");
+            return 0;
+        }
+        else {
+            endDate = document.getElementById("endDate").value;
+            if (endDate === "") {
+                alert("You did not pick a country! Error");
+                return 0;
+            }
+        }
+        
+        dateList = Object.keys(dailyCases); 
+        startIndex = dateList.findIndex(element => element === startDate);
+        endIndex = dateList.findIndex(element => element === endDate);
+        newDateList = dateList.slice(startIndex, endIndex + 1);
+        $.each(newDateList, function(index, value) {
+            newDailyCases[value] = dailyCases[value];
+            newDailyDeath[value] = dailyDeath[value];
+        });
+        paintGraph(newDailyCases, newCaseGraph);
+        paintGraph(newDailyDeath, newDeathGraph);
+    }
+
+    function updateTable() {
         var countryName, date;
         countryName = document.getElementById("country").value;
         if (countryName === "") {
